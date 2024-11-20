6e2ca684b0dceeaa18974d7b0e6f1dcd960449ad
Jennifer <jennifersoledad94@gmail.com>
2024-11-20 08:36:54 -0300

    push por los graficos

---
+++ src/Grafico/jTable1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grafico;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Usuario
+ */
+class jTable1 {
+
+    static void setModel(DefaultTableModel mt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
